57c97ca67a7f37d3c2a9c5828bf5ee4879848610
Sukhseerat Kaur <karanjitkaur78@gmail.com>
2021-06-14 13:38:40 +0530

    Initial Commit

---
+++ accept.py
@@ -0,0 +1,27 @@
+from time import sleep
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+ 
+driver = webdriver.Chrome(ChromeDriverManager().install())
+driver.get("https://www.linkedin.com/login")
+driver.maximize_window()
+
+
+driver.find_element_by_id("username").send_keys("karanjitkaur78@gmail.com")
+driver.find_element_by_id("password").send_keys("sukhsirat")
+driver.find_element_by_class_name("from__button--floating").click()
+
+driver.find_element_by_xpath("/html/body/div[5]/aside/div[1]/header/section[2]/button[2]").click()
+
+
+driver.get('https://www.linkedin.com/mynetwork/invitation-manager/')
+
+buttons = driver.find_elements_by_class_name("artdeco-button--secondary" and "invitation-card__action-btn") #elements for more than 1 elements
+# for button in buttons:
+#     aria_label_data = button.get_attribute("aria-label")
+#     if aria_label_data[0:6].lower() == "accept":
+#         button.click()
+for button in buttons:
+    button.click()
+driver.close()
+
